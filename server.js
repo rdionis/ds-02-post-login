d3f6d9da727da83ef1cce441be2dd9a561f32c38
Raquel Dionísio <dionisio.raquel@gmail.com>
2020-10-08 14:13:25 +0200

    figuring out exercise solution

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const app = express();
-const post = 5000;
+const post = 5001;
+
+app.use(express.json());
 
 let users = [
   { username: "User1", password: "PW1" },
   { username: "User2", password: "PW2" },
   { username: "User3", password: "PW3" },
 ];
+
+app.get("/", (req, res) => {
+  res.send("User logged in successfully");
+});
+
+app.post("/users", (req, res) => {
+  console.log(req.body);
+  res.send();
+  return { message: "User logged in successfully" };
+});
