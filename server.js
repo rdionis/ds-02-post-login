ea101c4921f7ecb45b891cfa3d966711a01029b6
Raquel Dionísio <dionisio.raquel@gmail.com>
2020-10-08 11:28:44 +0200

    Write a student user login

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const post = 5000;
+
+let users = [
+  { username: "User1", password: "PW1" },
+  { username: "User2", password: "PW2" },
+  { username: "User3", password: "PW3" },
+];
